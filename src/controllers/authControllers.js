4ee97efc2543b441c597c245570d7f59b300bc9a
hallstrom91 <simonhallstrom@live.se>
2024-10-17 23:01:43 +0200

    #11 - add controllers for new routes (login, register)

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,71 @@
+const { signJWT, validateJWT } = require("../utils/jwtUtils.js");
+const { hashPassword, verifyPassword } = require("../utils/bcryptjs.js");
+const pool = require("../utils/connectDB.js");
+
+const registerUser = async (req, res) => {
+  const { fullname, email, password } = req.body;
+
+  if (!fullname || !email || !password) {
+    return res
+      .status(400)
+      .json({ error: "Name, email or password value missing." });
+  }
+
+  try {
+    const [existingUser] = await pool.query(
+      "SELECT email FROM users WHERE email = ?",
+      [email],
+    );
+    if (existingUser.length > 0) {
+      return res.status(400).json({ error: "Email already in use" });
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    await pool.query(
+      "INSERT INTO users (fullname, email, password) VALUES (?, ?, ?)",
+      [fullname, email, hashedPassword],
+    );
+    return res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+const loginUser = async (req, res) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return res.status(400).json({ error: "Email or password value missing" });
+  }
+  try {
+    const [rows] = await pool.query("SELECT * FROM users WHERE email = ?", [
+      email,
+    ]);
+
+    if (rows.length === 0) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    const user = rows[0];
+
+    const isPasswordValid = await verifyPassword(password, user.password);
+
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Invalid email or password" });
+    }
+
+    const payload = {
+      userId: user.id,
+      email: user.email,
+      role: user.role,
+    };
+
+    const token = await signJWT(payload);
+    return res.status(200).json({ message: "Login successful", token });
+  } catch (error) {
+    return res.status(500).json({ error: "Internal server error" });
+  }
+};
+
+module.exports = { registerUser, loginUser };
