ec38473e0f0ceb4f9bf318bd6286bc95a1f1b983
hallstrom91 <simonhallstrom@live.se>
2024-10-17 23:00:56 +0200

    #10 - change logic and structure of authRoutes.js

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,35 +1,13 @@
 const express = require("express");
 const { signJWT, validateJWT } = require("../utils/jwtUtils.js");
-
+const {
+  loginUser,
+  registerUser,
+} = require("../controllers/authControllers.js");
 const router = express.Router();
 
-router.post("/sign", async (req, res) => {
-  const payload = req.body;
+router.post("/login", loginUser);
 
-  if (!payload || !payload.userId || !payload.email || !payload.role) {
-    return res.status(400).json({ error: "Invalid payload" });
-  }
-
-  try {
-    const token = signJWT(payload);
-    return res.status(200).json({ token });
-  } catch (error) {
-    return res.status(500).json({ error: error.message });
-  }
-});
-
-router.post("/verify", async (req, res) => {
-  const { token } = req.body;
-  if (!token) {
-    return res.status(400).json({ error: "Token missing" });
-  }
-
-  try {
-    const decoded = validateJWT(token);
-    return res.status(200).json({ message: "Token is valid", decoded });
-  } catch (error) {
-    return res.status(401).json({ error: error.message });
-  }
-});
+router.post("/register", registerUser);
 
 module.exports = router;
