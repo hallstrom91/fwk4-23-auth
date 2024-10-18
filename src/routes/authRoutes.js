fbc2589bb762e64639ce621a3474c9350c0f333d
hallstrom91 <simonhallstrom@live.se>
2024-10-18 14:12:32 +0200

    #17 - Added route for verifyjwt

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -3,11 +3,14 @@ const { signJWT, validateJWT } = require("../utils/jwtUtils.js");
 const {
   loginUser,
   registerUser,
+  verifyJwt,
 } = require("../controllers/authControllers.js");
 const router = express.Router();
 
 router.post("/login", loginUser);
 
 router.post("/register", registerUser);
+
+router.post("/verifyjwt", verifyJwt);
 
 module.exports = router;
