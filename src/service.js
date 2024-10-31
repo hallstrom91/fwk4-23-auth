0bdb05b3f28a1d04571e2985e4cedaf0ac60016c
Simon Hällström <simonhallstrom@live.se>
2024-10-31 16:22:50 +0100

    #40 - Add express-rate-limit

--- src/service.js
+++ src/service.js
@@ -1,15 +1,24 @@
-require("dotenv").config({ path: [".env.development.local", ".env"] });
 const express = require("express");
 const app = express();
 const helmet = require("helmet");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
+const rateLimit = require("express-rate-limit");
+
 const authRoutes = require("./routes/authRoutes.js");
 
 app.use(express.json());
 app.use(cookieParser());
 app.use(helmet());
 app.use(cors());
+
+const requestLimit = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: "To many requests, madman!",
+});
+
+app.use(requestLimit);
 
 app.use("/auth", authRoutes);
 
